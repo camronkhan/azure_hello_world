f4bed71c437f0fbc4324f04624af7fe7cbd5324e
Camron Khan <camron.khan@zurichna.com>
2017-12-14 15:56:21 -0600

    removed multer and pdf2json

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
-let pdfParser = require('pdf2json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
-router.get('/pdf', (req, res, next) => {
-
 });
 
 module.exports = router;
