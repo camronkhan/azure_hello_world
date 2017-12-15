66d1e42055b2e1540a5e66fa58c98011d469b9b3
Camron Khan <camron.khan@zurichna.com>
2017-12-15 12:34:52 -0600

    add port normalization

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const app = require('express')();
-const port = 3000;
+const port = normalizePort(process.env.PORT || '3000');
+function normalizePort(val) {
+    let port = parseInt(val, 10);
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+    return false;
+}
+app.set('port', port);
 app.get('/', (req, res) => res.send('Hello world on Azure!'));
 app.listen(port, () => console.log(`Server running on port ${port}`));
